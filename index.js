23ac439fcc8d15bf975feea30400cc50bb154abf
andrew <andrewasmit@hotmail.com>
2022-10-23 12:40:01 -0500

    commit tree abfe6a336f3b94c9aa90d37f3f481c64b87e1bbd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string){
+    const result = array.filter(driver => driver.toLowerCase() === string.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(array, string){
+    const result = array.filter(driver => driver[0].toLowerCase() === string[0].toLowerCase() );
+    return result;
+}
+
+function matchName(drivers, string){
+    const result = drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+    return result;
+}
